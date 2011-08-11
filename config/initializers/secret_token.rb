ab96d8e9f23a64079ecf92ef64c7b1adf291c2e6
sarathy sakshi <sarathys1@gmail.com>
2011-08-11 14:37:09 -0700

    hello world commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalcSvc::Application.config.secret_token = '9a41aa66f62f722cc57d26a3906ca177883d43fe63936e8515d3ced37dbd6cb9b8dfecd9ad0bbc5bd854879c00d50463b4bad05b03383ab38e6c46baaa3191c0'
